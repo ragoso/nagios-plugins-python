82463f18163506ff63aa0c32f1e2cf1688941572
Matheus Ragoso <matheusragoso@outlook.com>
2017-02-10 17:16:14 -0200

    Setting argv

--- check_dns.py
+++ check_dns.py
@@ -2,11 +2,12 @@
 
 ## pip install dnspython
 
+import sys
 import dns.resolver
 
 resolver = dns.resolver.Resolver()
-resolver.nameservers = ['8.8.8.8']
+resolver.nameservers = [sys.argv[1]]
 
-answer = resolver.query('google.com')
+answer = resolver.query(sys.argv[2])
 
 print(answer.rrset)
