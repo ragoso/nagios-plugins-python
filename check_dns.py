834177990a3375a667e8f0713789cf02e0ec1cd5
Matheus Ragoso <matheusragoso@outlook.com>
2017-02-10 18:45:30 -0200

    Modifications in check http and add usage function

--- check_dns.py
+++ check_dns.py
@@ -5,9 +5,16 @@
 import sys
 import dns.resolver
 
+import time
+if len(sys.argv) < 3:
+	print 'Usage: %s [server ip] [domain]' % __file__
+	sys.exit()
+
+
 resolver = dns.resolver.Resolver()
 resolver.nameservers = [sys.argv[1]]
 
+start_time = time.time()
 answer = resolver.query(sys.argv[2])
-
-print(answer.rrset)
+end_time = time.time()
+print('%s,  %s ms' % (answer.rrset, end_time - start_time))
