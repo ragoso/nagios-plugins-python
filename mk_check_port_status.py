8669a46b154f3044c7523c9a5af6c08472903261
Matheus Ragoso <matheusragoso@outlook.com>
2017-08-28 12:58:41 -0300

    Correção de MK Status e novo MK Traffic

--- mk_check_port_status.py
+++ mk_check_port_status.py
@@ -5,14 +5,17 @@ import os, sys
 if(len(sys.argv)<3):
 	print("Usage: ./{} [IP ADDRESS] [PORT OID]".format(sys.argv[0]))
 	exit(3)
-status = os.popen("/usr/bin/snmpget -v1 -c public -Cf {} {} | sed 's/.*INTEGER: //'".format(sys.argv[1], sys.argv[2]))
+
+oid_prefix = "1.3.6.1.2.1.2.2.1.8."
+oid_status = oid_prefix + sys.argv[2]
+status = os.popen("/usr/bin/snmpget -v1 -c public -Cf {} {} | sed 's/.*INTEGER: //'".format(sys.argv[1], oid_status))
 status = int(status.read())
 
-oid = sys.argv[2].split(".")
-oid[9] = '2'
-oid = ".".join(oid)
+oid_name = oid_status.split(".")
+oid_name[9] = '2'
+oid_name = ".".join(oid_name)
 
-name = os.popen("/usr/bin/snmpget -v1 -c public -Cf {} {} | sed 's/.*STRING: //'".format(sys.argv[1], oid))
+name = os.popen("/usr/bin/snmpget -v1 -c public -Cf {} {} | sed 's/.*STRING: //'".format(sys.argv[1], oid_name))
 name = name.read()
 
 p,r = "",3
